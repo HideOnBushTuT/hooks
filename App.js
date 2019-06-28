b8024b8b56f564991d462f1e4a23ce5cbc4b8e73
shanbin cai <shanbin.cai@gmail.com>
2019-06-28 17:18:15 +0800

    in show

--- App.js
+++ App.js
@@ -6,9 +6,9 @@
  * @flow
  */
 
-import React, {Component, useState, useEffect, useMemo } from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
-import { exportDefaultDeclaration } from '@babel/types';
+import React, { Component, useState, useEffect, useReducer } from 'react';
+import {Platform, StyleSheet, Text, View, Image, ScrollView, TextInput } from 'react-native';
+import Axios from 'axios';
 
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
@@ -17,37 +17,117 @@ const instructions = Platform.select({
     'Shake or press menu button for dev menu',
 });
 
-const Child = React.memo(({ name, content }) => {
-  console.log('child rerender');
-  changeName = (name) => {
-    console.log('Child');
-    return name + ' is changed'
+const fetchReducer = (state, action) => {
+  switch(action.type) {
+    case 'FETCH_INIT': 
+      return {
+        ...state,
+        isLoading: true
+      }
+    case 'FETCH_SUCCESS': 
+      return {
+        ...state,
+        isLoading: false,
+        data: action.payload,
+      }
+    case 'FETCH_FAILURE': 
+      return {
+        ...state,
+        isLoading: false,
+        isError: false
+      }
+    default: 
+      return state
   }
+}
 
-  const changedName = useMemo(() => changeName(name), [name])
+const useFetchData = (initialUrl) => {
+  // const [isError, setIsError] = useState(false)
+  // const [isLoading, setIsLoading] = useState(false)
+  // const [list, setList] = useState({ items: [] })
+  const [url, setUrl] = useState(initialUrl)
 
-  return (
-    <View style={{ flex:1, justifyContent: 'flex-start' }}>
-      <Text>{changedName}</Text>
-      <Text>{content}</Text>
-    </View>
-  );
+  const [state, dispatch] = useReducer(fetchReducer, {
+    data: { items: [] },
+    isLoading: false,
+    isError: false,
+  })
 
-})
+  useEffect(() => {
+    const fetchData = async () => {
+     try {
+       dispatch({ type: 'FETCH_INIT' })
+      //  setIsLoading(true)
+       const data = await Axios.get(url)
+       console.log('data', data)
+       dispatch({ type: 'FETCH_SUCCESS', payload: data.data })
+      //  setList(data.data)
+      //  setIsLoading(false) opvbv
+      //  setIsError(false)
+     } catch {
+        // setIsError(true)
+        // setIsLoading(false)
+        dispatch({ type: 'FETCH_FAILURE' })
+     }
+    }
+    fetchData()
+  }, [url])
 
+  return [state, setUrl]
+}
 
 const App = () => {
-  const [name, setName] = useState('name');
-  const [content, setContent] = useState('content');
-  
+  const [content, setContent] = useState('')
+  const [{data, isLoading, isError}, setUrl] = useFetchData('https://api.github.com/search/users?q=cbreno')
+
   return (
-    <View style={styles.container}>
-      <Text style={{ marginTop: 100 }} onPress={() => setName(new Date().getTime())}>{name}</Text>
-      <Text onPress={() => setContent('qwer')}>{content}</Text>
-      <Child name={name}>{content}</Child>
-    </View>
-  );
+      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        <View style={{ width: 300, height: 100, marginTop: 64, flexDirection: 'row', justifyContent: 'center', alignItems: 'center' }}>
+          <TextInput style={{ width: 200, height: 40 }} placeholder={'input here'} onChangeText={(text) => setContent(text)}/>
+          <Text onPress={() => setUrl(`https://api.github.com/search/users?q=${content}`)}>Click Me</Text>
+        </View>
+        <ScrollView>
+          {
+            isError ? <Text>Error</Text> : null
+          }
+        {
+          isLoading ? <Text>Loading</Text> : data.items.map(item => (
+            <View style={{ flexDirection: 'column', justifyContent: 'center', alignItems: 'center', width: 300, height: 200 }}>
+              <Text>{item.login}</Text>
+              <Image style={{ width: 100, height: 100 }} source={{ uri: item.avatar_url }} />
+            </View>
+          ))
+        }
+        </ScrollView>
+      </View>
+  )
 }
+
+// class App extends React.Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       content: 'aaaa'
+//     }
+
+//     this.handleChangeClick = this.handleChangeClick.bind(this)
+//   }
+
+//   handleChangeClick() {
+//     this.setState({ content: new Date().getTime() })
+//   }
+
+//   render() {
+//     return (
+//       <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+//         <Text onPress={this.handleChangeClick}>Click Me</Text>
+//         <Text>{this.state.content}</Text>
+//       </View>
+//     )
+//   }
+// }
+
+
 
 const styles = StyleSheet.create({
   container: {
