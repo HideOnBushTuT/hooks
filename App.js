dc06497e2c0d64e546df176e0362eacfc804dac7
shanbin cai <shanbin.cai@gmail.com>
2019-06-28 17:51:00 +0800

    code formatter

--- App.js
+++ App.js
@@ -7,7 +7,7 @@
  */
 
 import React, { Component, useState, useEffect, useReducer } from 'react';
-import {Platform, StyleSheet, Text, View, Image, ScrollView, TextInput } from 'react-native';
+import { Platform, StyleSheet, Text, View, Image, ScrollView, TextInput } from 'react-native';
 import Axios from 'axios';
 
 const instructions = Platform.select({
@@ -18,25 +18,25 @@ const instructions = Platform.select({
 });
 
 const fetchReducer = (state, action) => {
-  switch(action.type) {
-    case 'FETCH_INIT': 
+  switch (action.type) {
+    case 'FETCH_INIT':
       return {
         ...state,
         isLoading: true
       }
-    case 'FETCH_SUCCESS': 
+    case 'FETCH_SUCCESS':
       return {
         ...state,
         isLoading: false,
         data: action.payload,
       }
-    case 'FETCH_FAILURE': 
+    case 'FETCH_FAILURE':
       return {
         ...state,
         isLoading: false,
         isError: false
       }
-    default: 
+    default:
       return state
   }
 }
@@ -55,20 +55,20 @@ const useFetchData = (initialUrl) => {
 
   useEffect(() => {
     const fetchData = async () => {
-     try {
-       dispatch({ type: 'FETCH_INIT' })
-      //  setIsLoading(true)
-       const data = await Axios.get(url)
-       console.log('data', data)
-       dispatch({ type: 'FETCH_SUCCESS', payload: data.data })
-      //  setList(data.data)
-      //  setIsLoading(false) opvbv
-      //  setIsError(false)
-     } catch {
+      try {
+        dispatch({ type: 'FETCH_INIT' })
+        //  setIsLoading(true)
+        const data = await Axios.get(url)
+        console.log('data', data)
+        dispatch({ type: 'FETCH_SUCCESS', payload: data.data })
+        //  setList(data.data)
+        //  setIsLoading(false) opvbv
+        //  setIsError(false)
+      } catch {
         // setIsError(true)
         // setIsLoading(false)
         dispatch({ type: 'FETCH_FAILURE' })
-     }
+      }
     }
     fetchData()
   }, [url])
@@ -78,18 +78,18 @@ const useFetchData = (initialUrl) => {
 
 const App = () => {
   const [content, setContent] = useState('')
-  const [{data, isLoading, isError}, setUrl] = useFetchData('https://api.github.com/search/users?q=cbreno')
+  const [{ data, isLoading, isError }, setUrl] = useFetchData('https://api.github.com/search/users?q=cbreno')
 
   return (
-      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-        <View style={{ width: 300, height: 100, marginTop: 64, flexDirection: 'row', justifyContent: 'center', alignItems: 'center' }}>
-          <TextInput style={{ width: 200, height: 40 }} placeholder={'input here'} onChangeText={(text) => setContent(text)}/>
-          <Text onPress={() => setUrl(`https://api.github.com/search/users?q=${content}`)}>Click Me</Text>
-        </View>
-        <ScrollView>
-          {
-            isError ? <Text>Error</Text> : null
-          }
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <View style={{ width: 300, height: 100, marginTop: 64, flexDirection: 'row', justifyContent: 'center', alignItems: 'center' }}>
+        <TextInput style={{ width: 200, height: 40 }} placeholder={'input here'} onChangeText={(text) => setContent(text)} />
+        <Text onPress={() => setUrl(`https://api.github.com/search/users?q=${content}`)}>Click Me</Text>
+      </View>
+      <ScrollView>
+        {
+          isError ? <Text>Error</Text> : null
+        }
         {
           isLoading ? <Text>Loading</Text> : data.items.map(item => (
             <View style={{ flexDirection: 'column', justifyContent: 'center', alignItems: 'center', width: 300, height: 200 }}>
@@ -98,8 +98,8 @@ const App = () => {
             </View>
           ))
         }
-        </ScrollView>
-      </View>
+      </ScrollView>
+    </View>
   )
 }
 
